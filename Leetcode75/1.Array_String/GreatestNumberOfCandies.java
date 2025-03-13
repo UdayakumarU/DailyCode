46e8cfd3c6cf91f4faeea1342e7b80b59cc6f972
Udayakumar Udaiappa <31164995+UdayakumarU@users.noreply.github.com>
2025-03-13 14:00:52 +0530

    Create GreatestNumberOfCandies.java

---
+++ Leetcode75/1.Array_String/GreatestNumberOfCandies.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution{
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > maxCandies) {
+                maxCandies = candies[i];
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= maxCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+}
